c9fe9c50f61625d4bf6fec5d19a6c523d56389cc
Cameron Nathan <127004342+cmrnnathan@users.noreply.github.com>
2023-10-01 19:10:20 +1300

    extract student code from all files

--- LLM Testing/Code-Testing/src/main/java/FileContentReader.java
+++ LLM Testing/Code-Testing/src/main/java/FileContentReader.java
@@ -1,18 +1,41 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FileContentReader {
     public static String getFileContent(String repoPath) throws IOException {
-        String insuranceSystemPath = "/src/main/java/nz/ac/auckland/se281/InsuranceSystem.java";
-        String filePath = repoPath + insuranceSystemPath;
-
+        List<String> excludedFiles = List.of("Main.java", "MessageCli.java", "InsuranceSystem.java");
         StringBuilder content = new StringBuilder();
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+        // Get content of InsuranceSystem.java
+        String insuranceSystemPath = "/src/main/java/nz/ac/auckland/se281/InsuranceSystem.java";
+        String insuranceSystemFilePath = repoPath + insuranceSystemPath;
+
+        try (BufferedReader insuranceSystemReader = new BufferedReader(new FileReader(insuranceSystemFilePath))) {
             String line;
-            while ((line = reader.readLine()) != null) {
+            while ((line = insuranceSystemReader.readLine()) != null) {
                 content.append(line).append("\n");
+            }
+        }
+
+        // Iterate through other files and append their content
+        String srcPath = repoPath + "/src/main/java/nz/ac/auckland/se281";
+        File directory = new File(srcPath);
+        File[] files = directory.listFiles();
+
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile() && !excludedFiles.contains(file.getName()) && !file.getAbsolutePath().equals(insuranceSystemFilePath)) {
+                    try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                        String line;
+                        while ((line = reader.readLine()) != null) {
+                            content.append(line).append("\n");
+                        }
+                    }
+                }
             }
         }
 
